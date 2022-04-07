b38ea923d4269a3cea156d087f954567fa42c222
oldniu <1241677287@qq.com>
2022-04-07 22:24:26 +0800

    初始化路由并创建header和footer

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,62 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Index from "@/pages/Index.vue";
+import Login from "@/pages/Login.vue";
+import Detail from "@/pages/Detail.vue";
+import Edit from "@/pages/Edit.vue";
+import Create from "@/pages/Create.vue";
+import Register from "@/pages/Register.vue";
+import User from "@/pages/User.vue";
+import My from "@/pages/My.vue";
+import NotFound from "@/pages/NotFound.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    redirect: "/index",
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/index",
+    component: Index,
+  },
+  {
+    path: "/login",
+    component: Login,
+  },
+  {
+    path: "/detail",
+    component: Detail,
+  },
+  {
+    path: "/edit",
+    component: Edit,
+  },
+  {
+    path: "/create",
+    component: Create,
+  },
+  {
+    path: "/user",
+    component: User,
+  },
+  {
+    path: "/my",
+    component: My,
+  },
+  {
+    path: "/register",
+    component: Register,
+  },
+  {
+    path: "*",
+    component: NotFound,
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
