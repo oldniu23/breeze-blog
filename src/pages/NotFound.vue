e434cf9e36bd182a29a8f0f1110a8ffec89a4545
oldniu <1241677287@qq.com>
2022-04-07 22:58:10 +0800

    简单调整404页面

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>404页面</div>
+  <div id="notFound">
+    <h3>此页面不存在</h3>
+    <div id="jump">
+      <router-link to="/register"><p>点击跳转至注册页面</p></router-link>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +12,14 @@ export default {};
 </script>
 
 <style lang="less" scoped>
+#notFound {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+p {
+  padding: 50px;
+  font-size: 16px;
+}
 </style>
